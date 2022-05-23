32449e6f30054c4abba3604739b3c03d1cff1eb8
Mahathir-Gunjon <mahathirgunjon@gmail.com>
2022-05-24 02:35:33 +0600

    basic server setup and connect with mongodb

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000
+
+
+// middleWare
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.idddr.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
