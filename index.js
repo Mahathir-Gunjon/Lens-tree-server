cdb599c9f2f44e4132f8c18fc19665aa3396b601
Mahathir-Gunjon <mahathirgunjon@gmail.com>
2022-05-24 02:52:57 +0600

    items route created

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000
 
@@ -15,6 +14,27 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.idddr.mongodb.net/?retryWrites=true&w=majority`
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// start function start here
+async function start() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('products').collection('item')
+
+        app.get('/items', async (req, res) => {
+          const query = {};
+          const cursor = itemCollection.find(query)
+          const services = await cursor.toArray();
+          res.send(services)
+        })
+    } 
+    finally{
+
+    }
+}
+start() 
+
+// main route here | get all data
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
