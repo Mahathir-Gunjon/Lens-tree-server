184dd638fc5dbd5fe5fcc512d2c2d1823512105a
Mahathir-Gunjon <mahathirgunjon@gmail.com>
2022-05-25 20:20:00 +0600

    jwt verification function created

--- index.js
+++ index.js
@@ -15,83 +15,117 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 // start function start here
 async function start() {
-    try {
-        await client.connect();
-        const itemCollection = client.db('products').collection('item')
-        const reviewCollection = client.db('products').collection('review')
-        const orderCollection = client.db('products').collection('order')
-        const userCollection = client.db('products').collection('user')
+  try {
+    await client.connect();
+    const itemCollection = client.db('products').collection('item')
+    const reviewCollection = client.db('products').collection('review')
+    const orderCollection = client.db('products').collection('order')
+    const userCollection = client.db('products').collection('user')
 
-        app.get('/items', async (req, res) => {
-          const query = {};
-          const cursor = itemCollection.find(query)
-          const item = await cursor.toArray();
-          res.send(item)
-        })
+    app.get('/items', async (req, res) => {
+      const query = {};
+      const cursor = itemCollection.find(query)
+      const item = await cursor.toArray();
+      res.send(item)
+    })
 
-        app.get('/tool/:id', async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: ObjectId(id) };
-          const singleTool = await itemCollection.findOne(query);
-          res.send(singleTool);
-      })
+    app.get('/tool/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const singleTool = await itemCollection.findOne(query);
+      res.send(singleTool);
+    })
 
 
-        app.get('/review', async (req, res) => {
-          const query = {};
-          const cursor = reviewCollection.find(query)
-          const review = await cursor.toArray();
-          res.send(review)
-        })
+    app.get('/review', async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query)
+      const review = await cursor.toArray();
+      res.send(review)
+    })
 
-        app.get('/order', async (req, res) => {
-          const buyerEmail = req.query.buyerEmail;
-          const query = {buyerEmail: buyerEmail};
-          const orders = await orderCollection.find(query).toArray();
-          res.send(orders)
-        })
+    app.get('/order', verifyJWT, async (req, res) => {
+      const buyerEmail = req.query.buyerEmail;
+      const decodedEmail = req.decoded.email;
+      if (buyerEmail === decodedEmail) {
+        const query = { buyerEmail: buyerEmail };
+        const orders = await orderCollection.find(query).toArray();
+        return res.send(orders)
+      }
+      else{
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+    })
 
-        app.get('/user', async (req, res) => {
-          const users = await userCollection.find().toArray();
-          res.send(users)
-        })
-        
-        // post function start here 
+    app.get('/user', verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users)
+    })
 
-        app.post('/review', async (req, res) => {
-            const review = req.body;
-            await reviewCollection.insertOne(review);
-            res.send(review)
-        })
+    // post function start here 
 
-        app.post('/order', async (req, res) => {
-            const order = req.body;
-            await orderCollection.insertOne(order);
-            res.send(order)
-        })
+    app.post('/review', async (req, res) => {
+      const review = req.body;
+      await reviewCollection.insertOne(review);
+      res.send(review)
+    })
 
-        // put function start here
+    app.post('/order', async (req, res) => {
+      const order = req.body;
+      await orderCollection.insertOne(order);
+      res.send(order)
+    })
 
-        app.put('/user/:email', async (req, res) => {
-            const email = req.params.email;
-            const user = req.body;
-            const filter = { email: email };
-            const options = {upsert: true};
-            const updateDoc = {
-              $set: user,
-            };
-            const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1m'});
-            res.send({result, token});
-        })
-    } 
-    finally{
+    // put function start here
 
-    }
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m' });
+      res.send({ result, token });
+    })
+    
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m' });
+      res.send({ result, token });
+    })
+  }
+  finally {
+
+  }
 }
-start() 
+start()
 
 // main route here | get all data
 
